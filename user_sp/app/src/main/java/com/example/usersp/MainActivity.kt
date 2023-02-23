45d43d02feb9a0b576117b49af7f80e88020c92f
HugoRoca <hugo.rock20@hotmail.com>
2023-02-23 08:50:35 -0500

    using material card view

--- user_sp/app/src/main/java/com/example/usersp/MainActivity.kt
+++ user_sp/app/src/main/java/com/example/usersp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usersp.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
@@ -24,7 +26,28 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         val isFirstTime = preferences.getBoolean(getString(R.string.sp_first_time), true)
         Log.i("SP", "${getString(R.string.sp_first_time)} = $isFirstTime")
 
-        preferences.edit().putBoolean(getString(R.string.sp_first_time), false).commit()
+        if (isFirstTime) {
+            val dialogView = layoutInflater.inflate(R.layout.dialog_register, null)
+            MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.dialog_title)
+                .setView(dialogView)
+                .setCancelable(false)
+                .setPositiveButton(R.string.dialog_confirm) { dialogInterfcase, i ->
+                    val username = dialogView.findViewById<TextInputEditText>(R.id.etUserName).text.toString()
+
+                    with(preferences.edit()) {
+                        putBoolean(getString(R.string.sp_first_time), false).commit()
+                        putString(getString(R.string.sp_username), username).apply()
+                    }
+
+                    Toast.makeText(this, R.string.register_success, Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        } else {
+            val username = preferences.getString(getString(R.string.sp_username), getString(R.string.hint_username))
+            Toast.makeText(this, "Welcome $username", Toast.LENGTH_SHORT).show()
+        }
+
 
         userAdapter = UserAdapter(getUsers(), this)
         linearLayoutManager = LinearLayoutManager(this)
@@ -40,8 +63,8 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         val user = mutableListOf<User>()
 
         val alain = User(1, "Alain", "Nicolas", "https://via.placeholder.com/150")
-        val samant = User(2, "Samanta", "Meza", "https://via.placeholder.com/150")
-        val person1 = User(3, "Jehidi", "Chavez", "https://via.placeholder.com/150")
+        val samant = User(2, "Samanta", "Smith", "https://via.placeholder.com/150")
+        val person1 = User(3, "John", "Chavez", "https://via.placeholder.com/150")
         val person2 = User(4, "Hugo", "Roca", "https://via.placeholder.com/150")
 
         user.add(alain)
